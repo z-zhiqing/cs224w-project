3d51a0fa3efe1fd2cbf79948b5a843d1d1b89f31
Zilong Wang (Mike) <saberdoodle@gmail.com>
2018-12-08 20:46:47 -0800

    Adding utilities to load diffusion trees from file and compute sv

--- utilities.py
+++ utilities.py
@@ -1,3 +1,8 @@
+import os
+import snap
+import numpy as np
+import datetime
+import networkx as nx
 
 def split_activity_file(activityType):
     with open('raw_data/higgs-activity_time.txt') as data:
@@ -6,3 +11,18 @@ def split_activity_file(activityType):
                 start_id, end_id, timestamp, activity = item.split()
                 if activity == activityType:
                     f.write('%s %s %s %s\n' % (start_id, end_id, timestamp, activity))
+
+def loadTreesFromFile(dir):
+    trees = []
+
+    for filename in os.listdir(dir):
+        if filename.endswith(".txt"):
+            path = dir + filename
+            G_snap = snap.LoadEdgeList(snap.PNGraph, path, 0, 1)
+            G_networkx = nx.read_edgelist(path)
+            trees.append((G_snap,G_networkx))
+
+    return trees
+
+def getStructuralVirality(tree):
+    return nx.average_shortest_path_length(tree)
